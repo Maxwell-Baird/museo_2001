cba517dd7d22b680769ac854cdb714dc126e5275
Maxwell Baird <bairdm71@gmail.com>
2020-03-04 09:25:19 -0700

    test if you can add photos to curator

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -16,4 +16,24 @@ class CuratorTest < Minitest::Test
     assert_equal [], curator.photographs
     assert_equal [], curator.artists
   end
+
+  def test_it_can_add_photos
+    curator = Curator.new
+    assert_equal [], curator.photographs
+    photo1 = Photograph.new({
+      id: "1",
+      name: "Rue Mouffetard, Paris (Boy with Bottles)",
+      artist_id: "1",
+      year: "1954"
+     })
+     photo2 = Photograph.new({
+       id: "2",
+       name: "Moonrise, Hernandez",
+       artist_id: "2",
+       year: "1941"
+    })
+    curator.add_photograph(photo1)
+    curator.add_photograph(photo2)
+    assert_equal [photo1, photo2], curator.photographs
+  end
 end
