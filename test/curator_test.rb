f9555e2a018566236551500f1a8c523609840558
Maxwell Baird <bairdm71@gmail.com>
2020-03-04 09:21:41 -0700

    test the curator class has attributes

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -1,11 +1,19 @@
 require "minitest/autorun"
 require "minitest/pride"
 require './lib/curator'
+require './lib/photograph'
+require './lib/artist'
 
 class CuratorTest < Minitest::Test
 
   def test_it_exist
     curator = Curator.new
     assert_instance_of Curator, curator
+  end
+
+  def test_it_has_attributes
+    curator = Curator.new
+    assert_equal [], curator.photographs
+    assert_equal [], curator.artist
   end
 end
