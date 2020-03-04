996afd9c71f90e5cb9bab7ae3536acb6f0ea7d58
Maxwell Baird <bairdm71@gmail.com>
2020-03-04 09:37:05 -0700

    created a test to check photographs_by_artists

--- test/curator_test.rb
+++ test/curator_test.rb
@@ -79,4 +79,66 @@ class CuratorTest < Minitest::Test
     curator.add_artist(artist2)
     assert_equal artist1, curator.find_artist_by_id("1")
   end
+
+  def test_it_can_find_photographs_by_artist
+    curator = Curator.new
+    photo1 = Photograph.new({
+       id: "1",
+       name: "Rue Mouffetard, Paris (Boy with Bottles)",
+       artist_id: "1",
+       year: "1954"
+    })
+    photo2 = Photograph.new({
+       id: "2",
+       name: "Moonrise, Hernandez",
+       artist_id: "2",
+       year: "1941"
+    })
+    photo3 = Photograph.new({
+       id: "3",
+       name: "Identical Twins, Roselle, New Jersey",
+       artist_id: "3",
+       year: "1967"
+    })
+    photo4 = Photograph.new({
+       id: "4",
+       name: "Monolith, The Face of Half Dome",
+       artist_id: "3",
+       year: "1927"
+    })
+    artist1 = Artist.new({
+       id: "1",
+       name: "Henri Cartier-Bresson",
+       born: "1908",
+       died: "2004",
+       country: "France"
+    })
+    artist2 = Artist.new({
+       id: "2",
+       name: "Ansel Adams",
+       born: "1902",
+       died: "1984",
+       country: "United States"
+    })
+    artist3 = Artist.new({
+       id: "3",
+       name: "Diane Arbus",
+       born: "1923",
+       died: "1971",
+       country: "United States"
+    })
+    curator.add_artist(artist1)
+    curator.add_artist(artist2)
+    curator.add_artist(artist3)
+    curator.add_photograph(photo1)
+    curator.add_photograph(photo2)
+    curator.add_photograph(photo3)
+    curator.add_photograph(photo4)
+    expected = {
+      artist1 => [photo1],
+      artist2 => [photo2],
+      artist3 => [photo3, photo4]
+    }
+    assert_equal expected, curator.photographs_by_artist
+  end
 end
