88643f6320341d691b7939fb41f9506eeabd3eca
Maxwell Baird <bairdm71@gmail.com>
2020-03-04 09:44:16 -0700

    created photographs_by_artist method

--- lib/curator.rb
+++ lib/curator.rb
@@ -1,3 +1,6 @@
+require './lib/artist'
+require './lib/photograph'
+require 'pry'
 class Curator
 
   attr_reader :photographs, :artists
@@ -17,4 +20,17 @@ class Curator
   def find_artist_by_id(id)
     @artists.find{|artist| artist.id == id}
   end
+
+  def photographs_by_artist
+    by_artist = {}
+    @artists.each do |artist|
+      by_artist[artist] = []
+      by_artist[artist] << @photographs.find_all do|photograph|
+        photograph.artist_id == artist.id
+      end
+      by_artist[artist] = by_artist[artist].flatten
+    end
+    by_artist
+  end
+
 end
